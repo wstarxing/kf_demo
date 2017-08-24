9156a20337f3b7333a2454fc469e460d425426cf
wxx <wangxxxing@qq.com>
2017-08-24 17:41:53 +0800

    新增用户验证，待测

--- app/models/user.py
+++ app/models/user.py
@@ -1,14 +1,18 @@
 # -*- coding: UTF-8 -*-
+from flask import session
+from flask_login import login_user, logout_user, UserMixin
 from app.config.db import user
 from app.models.error import UserNotExists
 
 
-class User(object):
+class User(object, UserMixin):
     doc = user
 
-    def __init__(self, user_id=None):
+    def __init__(self, name=None, passwd=None, user_id=None):
         self.id = user_id
-        self.is_exists()
+        self.name = name
+        self.passwd = passwd
+        # self.is_exists()
 
     def get_user(self):
         pass
@@ -31,4 +35,35 @@ class User(object):
             print e
             raise UserNotExists
 
+    def is_veritied(self):
+        user = self.doc.user.find_one({'name': self.name})
+        if user.get('password', None) == self.passwd:
+            self.id = str(user['_id'])
+            self.login()
+            return True
+        else:
+            return False
 
+    @staticmethod
+    def is_authenticated():
+        return True
+
+    @staticmethod
+    def is_active():
+        return True
+
+    @staticmethod
+    def is_anonymous():
+        return True
+
+    def get_id(self):
+        return unicode(self.id)
+
+    def login(self):
+        login_user(self)
+
+    @staticmethod
+    def logout():
+        for i in session.keys():
+            session.pop(i)
+        logout_user()
--- app/views/demo/views.py
+++ app/views/demo/views.py
@@ -1,1 +1,20 @@
 # -*- coding: UTF-8 -*-
+from app.views.demo import demo
+from flask import jsonify, request
+from app.models.user import User
+from app.models.error import DataException
+@demo.route('/')
+def index():
+    return jsonify({'a': 1})
+
+
+@demo.route('/login', methods=['POST'])
+def login():
+    data = request.get_josn(force=True)
+    name = data.get('name', None)
+    password = data.get('passwd', None)
+    if name and password:
+        user = User(name, password)
+    else:
+        raise DataException
+
